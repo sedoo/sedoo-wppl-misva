4215398a5c5bdb83ff64eb9b0e31816f2abf8634
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-10-15 10:33:26 +0200

    Not working with product post type anymore, only with products from API

--- sedoo-wppl-misva-functions.php
+++ sedoo-wppl-misva-functions.php
@@ -13,3 +13,21 @@ function sedoo_blocks_components_render_callback_misva( $block ) {
 		include $templateURL;
     }
 }
+
+//////
+// Fill products field on block with data's from API
+/////
+function sedoo_misva_fill_product_field( $field ) {
+	$products_list_api = file_get_contents("https://services.aeris-data.fr/misva/data/v1_0/products");
+	$products_list_in_array = json_decode($products_list_api, true);
+	
+	foreach($products_list_in_array as $product_in_array) {
+		/// Proposé au format wind : Wind
+		$products_ready_for_field[$product_in_array['id']] = $product_in_array['name'] ;
+	}
+	
+	$field['choices'] = $products_ready_for_field;
+	return $field;
+}
+
+add_filter('acf/load_field/name=produits_a_afficher', 'sedoo_misva_fill_product_field');
